79dbeb1d1c27aa72e4ea7d8293d9de11d59fc53a
IanGaffney <35350577+IanGaffney@users.noreply.github.com>
2018-04-24 16:07:51 -0700

    4/14 EOD commit

--- CST126_2/stock.cpp
+++ CST126_2/stock.cpp
@@ -2,4 +2,18 @@
 // stock.cpp
 //--------------------------------------------------------------------
 #include "stdafx.h"
+#include "stock.h""
+
+Stock::Stock() :
+	m_symbol{ "" }, m_name{ "" }, m_price{ 0.0 }, m_earnings{0.0}
+{
+}
+Stock::Stock(std::string symbol, std::string name, double price, double earnings) :
+	m_symbol{ symbol }, m_name{ name }, m_price{ price }, m_earnings{ earnings }
+{
+}
+
+Stock::~Stock()
+{
+}
 
--- CST126_2/stock.h
+++ CST126_2/stock.h
@@ -2,3 +2,21 @@
 // stock.h
 //     
 //--------------------------------------------------------------------
+#include <iostream>
+
+class Stock {
+public:
+	Stock();
+	Stock(std::string symbol, std::string name, double price, double earnings);
+	~Stock();
+	std::string getSymbol() const { return m_symbol; }
+	std::string getName() const { return m_name; }
+	double getPrice() const { return m_price; }
+	double getEarnings() const { return m_earnings; }
+
+private:
+	std::string m_symbol;
+	std::string m_name;
+	double m_price;
+	double m_earnings;
+};
