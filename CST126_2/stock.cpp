0a875f911ff876ba24fba0b0c56bc11b5c2262f8
IanGaffney <35350577+IanGaffney@users.noreply.github.com>
2018-04-25 16:04:17 -0700

    4/25 EOD checkin

--- CST126_2/stock.cpp
+++ CST126_2/stock.cpp
@@ -2,10 +2,15 @@
 // stock.cpp
 //--------------------------------------------------------------------
 #include "stdafx.h"
-#include "stock.h""
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "stock.h"
+
+using namespace std;
 
 Stock::Stock() :
-	m_symbol{ "" }, m_name{ "" }, m_price{ 0.0 }, m_earnings{0.0}
+	m_symbol{}, m_name{}, m_price{}, m_earnings{}
 {
 }
 Stock::Stock(std::string symbol, std::string name, double price, double earnings) :
@@ -17,3 +22,15 @@ Stock::~Stock()
 {
 }
 
+void Stock::printStock()
+{
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.setf(ios::left);
+	cout.precision(2);
+	cout << setw(4) << m_symbol << " "
+		 << setw(25) << m_name << " "
+		 << "Price: " << setw(9) << m_price << " "
+		 << "Earnings " << setw(6) << m_earnings << " "
+		 << "P/E " << getPricePerEarnings() << "\n";
+}
--- CST126_2/stock.h
+++ CST126_2/stock.h
@@ -4,6 +4,9 @@
 //--------------------------------------------------------------------
 #include <iostream>
 
+#ifndef H_STOCK
+#define H_STOCK
+
 class Stock {
 public:
 	Stock();
@@ -11,12 +14,18 @@ public:
 	~Stock();
 	std::string getSymbol() const { return m_symbol; }
 	std::string getName() const { return m_name; }
-	double getPrice() const { return m_price; }
+	double getPrice() { return m_price; }
 	double getEarnings() const { return m_earnings; }
+	void adjustStockPrice(double adjustPercent) { m_price = (m_price / 100.0) * (100.0 + adjustPercent); }
+	
+	void printStock();
 
 private:
 	std::string m_symbol;
 	std::string m_name;
 	double m_price;
 	double m_earnings;
+	double getPricePerEarnings() { return m_price / fabs(m_earnings); }
 };
+
+#endif /* H_STOCK */
