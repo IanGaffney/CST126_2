2a043b65bba732ff0d9d7eb9502a8ec4927d5313
IanGaffney <35350577+IanGaffney@users.noreply.github.com>
2018-05-01 16:02:01 -0700

    Completed

--- CST126_2/stock.h
+++ CST126_2/stock.h
@@ -12,12 +12,9 @@ public:
 	Stock();
 	Stock(std::string symbol, std::string name, double price, double earnings);
 	~Stock();
-	std::string getSymbol() const { return m_symbol; }
-	std::string getName() const { return m_name; }
 	double getPrice() { return m_price; }
-	double getEarnings() const { return m_earnings; }
-	void adjustStockPrice(double adjustPercent) { m_price = (m_price / 100.0) * (100.0 + adjustPercent); }
-	
+	void setPrice(double price) { m_price = price; }
+	void adjustStockPrice(double adjustPercent) { setPrice((m_price / 100.0) * (100.0 + adjustPercent)) ; }
 	void printStock();
 
 private:
